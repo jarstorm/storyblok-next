e8b1d30c81030410876ff427763030528d8d3876
Javier Abad <javier.abad@emurgo.io>
2023-03-24 22:09:07 +0100

    Improve images performance

--- components/Post.js
+++ components/Post.js
@@ -1,11 +1,16 @@
 import Link from "next/link";
+import Image from 'next/image'
 
 const Post = ({title, img, body, id}) => {          
 const postLink = `/post/${id}`;
   return (
         <div className="border p-10 m-5 w-72 rounded-2xl">
             <span className="block mb-2 text-sm text-red-900">{title}</span>    
-            <img src={img} className={"w-58 h-auto mb-2"}/>
+            <Image
+              src={img}
+              width={500}
+              height={500}              
+            />            
             <span className="block text-sm text-green-900">{body}</span>
             <Link href={`/post/${id}`}>Open</Link>
         </div>
---
+++ next.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    images: { domains: ['a.storyblok.com'], 
+    formats: ['image/avif', 'image/webp'], }
+ }
